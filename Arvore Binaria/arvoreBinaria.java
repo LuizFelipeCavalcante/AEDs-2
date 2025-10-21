cb22dd0b309e309bf5e89f52741a1bf03a1abbfc
LuizFelipeCavalcante <luizf.a.cavalcante@gmail.com>
2025-10-21 17:24:04 -0300

    update

--- Arvore Binaria/arvoreBinaria.java
+++ Arvore Binaria/arvoreBinaria.java
@@ -1,3 +1,47 @@
 public class arvoreBinaria {
-    
+    private No raiz;
+    private class No{
+        public int val;
+        public No esq;
+        public No dir;
+
+        public void arvoreBinaria(int val){this.val = val;}
+    }
+
+    private No inserirRec(No visitando, int val){
+        
+        if(visitando == null){
+            visitando.val = val;
+            this.raiz.val = val;
+            return visitando;
+        }
+        else{
+            if(visitando.val < val){
+                visitand.dir = inserirRec(visitando.dir, val);
+            }else{
+                visitando.esq = inserirRec(visitando.esq, val);
+            }
+        }
+        
+        
+        return visitando;
+    }
+
+    public void inserir(int val){
+    inserirRec(this.Raiz, val);
+    }
+
+    private void imprimirRec(No visitando){
+        if(visitando == null){
+            return;
+        }
+        else{
+            if(visitando.esq == null){
+                System.out.print(visitando);
+                imprimirRec(visitando.dir);
+            }
+        }
+    }
+
+    private 
 }
---
+++ TP4/Game.java
@@ -0,0 +1,9 @@
+public class Game{
+
+    private int id;
+    private String name;
+    private String releaseDate;
+    private int estimatedOwners;
+    private float price;
+    private String[] supportedLanguages
+}
