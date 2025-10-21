8a33f1cbba55ea761f8e70761ca275f3add436f5
LuizFelipeCavalcante <luizf.a.cavalcante@gmail.com>
2025-10-21 17:43:45 -0300

    feat: adicionar base de classe game incompleta

--- TP4/Game.java
+++
@@ -1,9 +0,0 @@
-public class Game{
-
-    private int id;
-    private String name;
-    private String releaseDate;
-    private int estimatedOwners;
-    private float price;
-    private String[] supportedLanguages
-}
---
+++ TP4/TP4Game.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class Game{
+
+    private int id;
+    private String name;
+    private String releaseDate;
+    private int estimatedOwners;
+    private float price;
+    private String[] supportedLanguages;
+    private int metacriticScore;
+    private float userScore;
+    private int achievements;
+    private String[] publishers;
+    private String[] developers;
+    private String[] categories;
+    private String[] genres;
+    private String[] tags;
+
+    public void setId(string id){
+        this.id = id;
+    }
+    
+    public void setName(string name){
+        this.name = name;
+    }
+    
+    public void setReleaseDate(string releaseDate){
+        this.releaseDate = releaseDate;
+    }
+    
+    public void setEstimatedOwners(string estimatedOwners){
+        this.estimatedOwners = estimatedOwners;
+    }
+    
+    public void setPrice(string price){
+        this.price = price;
+    }
+    
+    public void setSupportedLanguages(string supportedLanguages){
+        this.supportedLanguages = supportedLanguages;
+    }
+    
+    public void setMetacriticScore(string metacriticScore){
+        this.metacriticScore = metacriticScore;
+    }
+    
+    public void setUserScore(string userScore){
+        this.userScore = userScore;
+    }
+    
+    public void setAchievement(string achievements){
+        this.achievements = achievements;
+    }
+    
+    public void setId(string id){
+        this.id = id;
+    }
+    
+    public void setId(string id){
+        this.id = id;
+    }
+    
+    public void setId(string id){
+        this.id = id;
+    }
+    
+    public void setId(string id){
+        this.id = id;
+    }
+    
+    public void setId(string id){
+        this.id = id;
+    }
+    
+    public void setId(string id){
+        this.id = id;
+    }
+    
+    public void setId(string id){
+        this.id = id;
+    }
+}
+
+public class TP4Game{
+    public static void main(String[] args) {
+        System.in.read();
+    }
+}
