a985303de7ed8d2d3fcfe92bbd1562d8fd913cf2
LuizFelipeCavalcante <luizf.a.cavalcante@gmail.com>
2025-11-02 09:53:28 -0300

    feat: arvore binaria incompleta

--- Arvore Binaria/arvoreBinaria.java
+++ Arvore Binaria/arvoreBinaria.java
@@ -31,17 +31,42 @@ public class arvoreBinaria {
     inserirRec(this.Raiz, val);
     }
 
-    private void imprimirRec(No visitando){
-        if(visitando == null){
-            return;
-        }
-        else{
-            if(visitando.esq == null){
-                System.out.print(visitando);
-                imprimirRec(visitando.dir);
-            }
+    private void imprimirRecCentral(No visitando){
+        if(visitando != null){
+            imprimirRecCentral(visitando.esq);
+            System.out.println(visitante.elemento + " ");
+            imprimirRecCentral(visitando.dir);
         }
     }
 
-    private 
+    private void imprimirRecPos(No visitando){
+        if(visitando != null){
+            imprimirRecPos(visitando.esq);
+            imprimirRecPos(visitando.dir);
+            System.out.println(visitante.elemento + " ");
+        }
+    }
+
+    private void imprimirRecPre(No visitando){
+        if(visitando != null){
+            System.out.println(visitante.elemento + " ");
+            imprimirRecPre(visitando.esq);
+            imprimirRecPre(visitando.dir);
+        }
+    }
+
+    private int alturaArvore(No i, int altura){
+        if(i == null){
+            return altura--;
+        }else{
+            int alturaEsq = alturaArvore(i.esq, altura + 1);
+            int alturaDir = alturaArvore(i.dir, altura + 1);
+            altura = (alturaEsq > alturaDir) ? alturaEsq : alturaDir;
+            return altura;
+        }   
+        
+        return 0;
+    }
+
+    
 }
